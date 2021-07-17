965e8757ed3c96d0ea2580170bbe6b9972a5aec5
maru44 <takumaru22244@gmail.com>
2021-07-18 07:49:16 +0900

    testable clean arch

--- articles/bad.go
+++ articles/bad.go
@@ -7,17 +7,37 @@ var (
 	water = "water"
 )
 
-type Transfer interface {
-	transferWhite(string, int) int
-	transferGreen(string, int) int
+type transferR interface {
 	transferRed(string, int) int
+}
+
+type transferG interface {
+	tranferGreen(string, int) int
+}
+
+type transferW interface {
+	transferWhite(string, int) int
 }
 
 type MobileSuit struct {
 	weight int
 	engine int
 	name   string
-	Transfer
+}
+
+type red struct {
+	MobileSuit
+	transferR
+}
+
+type green struct {
+	MobileSuit
+	transferG
+}
+
+type white struct {
+	MobileSuit
+	transferW
 }
 
 // 速度算出
@@ -43,48 +63,54 @@ func (m *MobileSuit) transferZaku(field string, seconds int) int {
 }
 
 // 白い悪魔移動距離算出
-func (m *MobileSuit) transferWhite(field string, seconds int) int {
+func (w *white) transferWhite(field string, seconds int) int {
 	var transfer int
 	switch field {
 	case space:
-		index := m.calculateSpeedIndex()
+		index := w.calculateSpeedIndex()
 		transfer = int(index * 1.2 * float32(seconds))
 	case water:
-		index := m.calculateSpeedIndex()
+		index := w.calculateSpeedIndex()
 		transfer = int(index * .7 * float32(seconds))
 	default:
-		index := m.calculateSpeedIndex()
+		index := w.calculateSpeedIndex()
 		transfer = int(index * float32(seconds))
 	}
 	return transfer
 }
 
 // 移動距離算出
-func (m *MobileSuit) transferGreen(field string, seconds int) int {
-	return m.transferZaku(field, seconds)
+func (g *green) transferGreen(field string, seconds int) int {
+	return g.transferZaku(field, seconds)
 }
 
 // 移動距離算出
-func (m *MobileSuit) transferRed(field string, seconds int) int {
-	return m.transferZaku(field, seconds)
+func (r *red) transferRed(field string, seconds int) int {
+	return r.transferZaku(field, seconds)
 }
 
 func main() {
 	field := space
-	white := &MobileSuit{
-		weight: 43,
-		engine: 55,
-		name:   "白い悪魔",
+	white := &white{
+		MobileSuit: MobileSuit{
+			weight: 43,
+			engine: 55,
+			name:   "白い悪魔",
+		},
 	}
-	green := &MobileSuit{
-		weight: 58,
-		engine: 43,
-		name:   "緑の脇役",
+	green := &green{
+		MobileSuit: MobileSuit{
+			weight: 58,
+			engine: 43,
+			name:   "緑の脇役",
+		},
 	}
-	red := &MobileSuit{
-		weight: 58,
-		engine: 43,
-		name:   "赤いの",
+	red := &red{
+		MobileSuit: MobileSuit{
+			weight: 58,
+			engine: 43,
+			name:   "赤いの",
+		},
 	}
 	transferWhite := white.transferWhite(field, 200)
 	transferGreen := green.transferGreen(field, 200)
